8c2d23c4a5acb27fb964629cfab428f419e21e7b
akash-685603 <skyme5997@gmail.com>
2024-12-12 18:29:20 +0530

    changed vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,10 @@
 export default {
+  root: './frontend', // Specify the root directory where your index.html is located
+  build: {
+    outDir: '../dist', // Output directory for the build (adjust as needed)
+  },
   server: {
     port: 5173,  // Set the port to 5173
-  }
+  },
 }
+
